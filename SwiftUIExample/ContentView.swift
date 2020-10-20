52394738d23c60c766b7ed841ca8ccd0a6a1e5c5
Jack Walters <jackwalters784@gmail.com>
2020-10-20 17:46:40 -0400

    initial commit for example swift ui project

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -10,9 +10,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUP: true)
+            }
+        }
+            .padding()
+            .foregroundColor(.orange)
+            .font(Font.largeTitle)
+        
+        
     }
 }
+
+struct CardView: View {
+    
+    var isFaceUP: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUP {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
