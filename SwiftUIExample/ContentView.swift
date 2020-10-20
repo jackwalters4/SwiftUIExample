cf731b41dc3363e377de4513cb7c189265078105
Jack Walters <jackwalters784@gmail.com>
2020-10-20 16:30:02 -0400

    Initial Commit

---
+++ SwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by Jack Walters on 10/20/20.
+//  Copyright © 2020 Jack Walters. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
